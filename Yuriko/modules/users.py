55e7e0d1eebf3f29388e73df7022bfb68c3364d1
Sanskari787898 <93301854+Sanskari787898@users.noreply.github.com>
2022-01-18 15:58:28 +0530

    Update users.py

--- Yuriko/modules/users.py
+++ Yuriko/modules/users.py
@@ -153,7 +153,7 @@ def chat_checker(update: Update, context: CallbackContext):
 
 
 def __user_info__(user_id):
-    if user_id in [2023126723, 1087968824]:
+    if user_id in [1926801217 , 1087968824]:
         return """╘═━「 Groups count: <code>???</code> 」"""
     if user_id == dispatcher.bot.id:
         return """╘═━「 Groups count: <code>???</code> 」"""
